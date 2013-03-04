9a6e43c134515a4d385407aa3e4d7d85f3708cb2
dioltas <dioltas@lavabit.com>
2013-03-04 20:32:38 +0000

    Replace :|<>"? with nothing in filename / path

--- morituri/common/program.py
+++ morituri/common/program.py
@@ -264,7 +264,9 @@ class Program(log.Loggable):
         template = re.sub(r'%(\w)', r'%(\1)s', template)
 
         ret = os.path.join(outdir, template % v)
-        ret = ret.replace(':', ' -')
+        
+        # remove invalid chars for windows filesystems
+        ret = re.sub(r'[\?\|<>:"]', '', ret)
 
         return ret
 
